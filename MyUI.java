71f76a90c1fd0d2ce0e2f0dbe007b1a486a0f03b
eswaribala <Parameswaribala@gmail.com>
2018-03-22 16:20:13 +0530

    Add files via upload

--- MyUI.java
+++ MyUI.java
@@ -1,22 +1,18 @@
-package com.cgi.travel.controllers;
-
-import java.util.List;
+package com.example.wisordemoproject;
 
 import javax.servlet.annotation.WebServlet;
 
-import com.cgi.travel.models.Flight;
+
+import com.example.views.ASRView;
+import com.example.views.SOWAView;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.icons.VaadinIcons;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.ClassResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.shared.ui.ValueChangeMode;
-import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CssLayout;
-import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
@@ -25,84 +21,80 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
-@SpringUI(path="/ui")
-@Theme("valo")
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of an HTML page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("mytheme")
 public class MyUI extends UI {
 
-	private FlightService customerService=FlightService.getInstance();
-	//private Grid<Customer> grid=new Grid<>(Customer.class);
-	//private TextField filterText =new TextField();
-	private FlightForm flightForm=new FlightForm(this);
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-    	
-    	Image image = new Image("CGI logo:",
-    		    new ClassResource("kingfisher.jpg"));
-    	image.setWidth("100px");
-    	image.setHeight("100px");
-    	
-    	final VerticalLayout logoLayout = new VerticalLayout();
-    	logoLayout.addComponent(image);
-    	 final VerticalLayout layout = new VerticalLayout();
-    	/*
-       
-        filterText.setPlaceholder("Filter by Name");
-        filterText.addValueChangeListener(e->updateList());
-        filterText.setValueChangeMode(ValueChangeMode.LAZY);
-        */
-        Button addFlightBtn = new Button("Add new Flight");
-        addFlightBtn.addClickListener(e -> {
-            //grid.asSingleSelect().clear();
-            flightForm.setFlight(new Flight());
-        });
-        /*
-        Button clearTextBtn=new Button(VaadinIcons.CLOSE);
-        clearTextBtn.setDescription("Clear Filter Text");
-        clearTextBtn.addClickListener(e->filterText.clear());
-        CssLayout filteringLayout=new CssLayout();
-        filteringLayout.addComponents(filterText,clearTextBtn);
-        filteringLayout.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
-        */
-        HorizontalLayout toolbar = new HorizontalLayout(logoLayout,addFlightBtn);
-     
-        //grid.setColumns("firstName", "lastName", "email");
-        HorizontalLayout main = new HorizontalLayout(flightForm);
-       
-        main.setSizeFull();
-        //grid.setSizeFull();
-        //main.setExpandRatio(grid, 1);
-
+    	Image logo = new Image("",
+    		    new ClassResource("bell.png"));
+    	logo.setWidth("100px");
+    	logo.setHeight("100px");
+        Label menuHeading =new Label("CSG View");
+        menuHeading.addStyleName(ValoTheme.MENU_TITLE);
         
-        layout.addComponents(toolbar,main);
-        updateList();
-        /*
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
-
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
+        Button asr=new Button("ASR");
+      
+        asr.addStyleNames(ValoTheme.BUTTON_LINK,ValoTheme.MENU_ITEM);
+        
+        Button sowa=new Button("SOWA");
+        sowa.addStyleNames(ValoTheme.BUTTON_LINK,ValoTheme.MENU_ITEM);
+        Button csr=new Button("CSR");
+        csr.addStyleNames(ValoTheme.BUTTON_LINK,ValoTheme.MENU_ITEM);
+        Button provisioning=new Button("PROVISIONING");
+        provisioning.addStyleNames(ValoTheme.BUTTON_LINK,ValoTheme.MENU_ITEM);
+        Button tables=new Button("TABLES");
+        tables.addStyleNames(ValoTheme.BUTTON_LINK,ValoTheme.MENU_ITEM);
+        Button reports=new Button("REPORTS");
+        reports.addStyleNames(ValoTheme.BUTTON_LINK,ValoTheme.MENU_ITEM);
+        Button userprefs=new Button("USERPREFS");
+        userprefs.addStyleNames(ValoTheme.BUTTON_LINK,ValoTheme.MENU_ITEM);
+        Image applnLogo = new Image("",
+    		    new ClassResource("owl.jpg"));
+        applnLogo.setWidth("100px");
+        applnLogo.setHeight("100px");
+        Button close=new Button("CLOSE");
+        close.addStyleNames(ValoTheme.BUTTON_DANGER);
+        VerticalLayout vlayout=new VerticalLayout();
+        CssLayout menulayout=new CssLayout();        
+        menulayout.addComponents(menuHeading,asr,sowa,csr,provisioning,tables,reports,userprefs);
+        vlayout.addComponents(logo,menulayout,applnLogo,close);
+        CssLayout container=new CssLayout();
+        HorizontalLayout page=new HorizontalLayout();
+        page.addComponents(vlayout,container);
+        
+        Navigator navigator = new Navigator(this,container);
+        navigator.addView(ASRView.NAME, new ASRView());
+        navigator.addView(SOWAView.NAME,new SOWAView());
+        navigator.addView("", new ASRView());
+        
+        navigator.navigateTo(ASRView.NAME);
+        asr.addClickListener(event->{
+        	
+        	
+        	navigator.navigateTo(ASRView.NAME);
+        	
+        });
+ sowa.addClickListener(event->{
+        	
+        	
+        	navigator.navigateTo(SOWAView.NAME);
+        	
         });
         
-        layout.addComponents(name, button);
-        */
-        setContent(layout);
-        flightForm.setVisible(false);
-        /*
-        grid.asSingleSelect().addValueChangeListener(e->{
-        	if(e.getValue()==null)
-        		customerForm.setVisible(false);
-        	else
-        		customerForm.setCustomer(e.getValue());
-        });
-        */
+        
+        setContent(page);
     }
 
-	public void updateList() {
-		//List<Customer> customers=customerService.findAll(filterText.getValue());
-       // grid.setItems(customers);
-	}
-
-    
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
 }
